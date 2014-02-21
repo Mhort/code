5fd079b144ce651a0a9acc0ee7b59a178637fa93
Martin Crean <2107094C@student.gla.ac.uk>
2014-02-21 21:21:00 +0000

    started ch4

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -2,7 +2,12 @@ import os
 SETTINGS_DIR = os.path.dirname(__file__)
 PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+TEMPLATE_PATH = os.path.join(
+    PROJECT_PATH, 'templates)'
+)
 # Django settings for tango_with_django_project project.
+
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -111,11 +116,12 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'H;/code/code/tango_with_django_project/templates/'
+    TEMPLATE_PATH,
 )
 
 INSTALLED_APPS = (
