915c07a3c9a3281e69746cf4df10b68665260ab4
Martin Crean <2107094C@student.gla.ac.uk>
2014-03-04 23:29:01 +0000

    Ch 7 comp

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -4,4 +4,6 @@ from rango import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
-    url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),)  # New!
+    url(r'^add_category/$', views.add_category, name='add_category'), # NEW MAPPING!
+    # Maps the category page urls (rango, django, other frameworks)
+    url(r'^category/(?P<category_name_url>\w+)$', views.category, name='category'),)
